63d5b91775b9b24e2d520966066beda57c228753
Jayeon Kim <63771579+nature1216@users.noreply.github.com>
2025-08-10 11:32:33 +0900

    =[level 2] 할인 행사_김자연.java

---
+++ 11회/[level 2] 할인 행사_김자연.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    static Map<String, Integer> window = new HashMap<>();
+    
+    public boolean check(String[] want, int[] number) {
+        for(int j=0;j<want.length;j++) {
+            int cnt = window.getOrDefault(want[j], 0);
+            if(cnt < number[j]) return false;
+        }
+        return true;
+    }
+    
+    public int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+        int size = 10;
+        
+        for(int i=0;i<size;i++) {
+            int cnt = window.getOrDefault(discount[i], 0);
+            window.put(discount[i], cnt+1);
+        }
+        
+        for(int i=9;i<discount.length;i++) {
+            if(check(want, number)) answer++;
+            if(i+1 >= discount.length) break;
+            
+            window.put(discount[i-9], window.get(discount[i-9]) - 1);
+            window.put(discount[i+1], window.getOrDefault(discount[i+1], 0) + 1);
+        }
+        
+        return answer;
+    }
+}
