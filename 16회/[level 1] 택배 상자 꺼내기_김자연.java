f7e4e618b408729dc766d1779d0d840f1e1537d6
Jayeon Kim <63771579+nature1216@users.noreply.github.com>
2025-09-14 11:50:40 +0900

    [level 1] 택배 상자 꺼내기_김자연.java

--- 16회/[level 1] 택배 상자 꺼내기_김자연.java
+++ 16회/[level 1] 택배 상자 꺼내기_김자연.java
@@ -1,8 +1,10 @@
 class Solution {
     public int solution(int n, int w, int num) {
+        // 꺼낼박스좌표
         int tx = ((num-1)/w) % 2 == 0 ? (num-1)%w : w - ((num-1)%w) - 1;
         int ty = (num-1)/w;
-        
+
+        // 마지막박스좌표
         boolean flag = ((n-1)/w) % 2 == 0;
         int topx = flag ? (n-1)%w : w - ((n-1)%w) - 1;
         int topy = (n-1)/w;
