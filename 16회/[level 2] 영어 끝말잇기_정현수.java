c0244b055f1fb14e6a27d094580aff5dfec5aff4
hs20180519 <108255695+hs20180519@users.noreply.github.com>
2025-09-14 10:53:08 +0900

    [level 2] 영어 끝말잇기_정현수.java

---
+++ 16회/[level 2] 영어 끝말잇기_정현수.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n, String[] words) {
+        int[] answer = {};
+        HashSet<String> hset = new HashSet<>();
+        int len = words.length;
+
+        int idx = 0;
+        for(int j=0; j<len/n; j++){ // 3
+            for(int i=0; i<n; i++){ // 5
+                // 이미 있는 단어거나, 전에 한 문자인 경우
+                
+                if( hset.contains(words[idx]) || (idx > 0) &&
+                  !(words[idx].startsWith(Character.toString(words[idx-1].charAt(words[idx-1].length()-1))))){
+                    // i번째 사람의 j번째
+                    return new int[]{i+1, j+1};
+                
+                }else{
+                    hset.add(words[idx++]);
+                    // endGame[i][j] = words[idx++];
+                }
+            }
+        }
+
+
+        return new int[]{0, 0};
+    }
+}
