0ab4ee71df8edc9b57a62bc95f13e3d8a78f66ee
Jayeon Kim <63771579+nature1216@users.noreply.github.com>
2025-09-22 11:07:16 +0900

    Create

---
+++ 17회/[level 2] 비밀 코드 해독_김자연.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    static int answer;
+    public void comb(int start, int depth, int selected[], int n, int[][] q, int[] ans) {
+        if(depth == 5) {
+            Set<Integer> set = new HashSet<>();
+            for(int i=0;i<selected.length;i++) {
+                set.add(selected[i]);
+            }
+            for(int i=0;i<q.length;i++) {
+                int cnt = 0;
+                for(int j=0;j<5;j++) {
+                    if(set.contains(q[i][j])) cnt++;
+                }
+                if(cnt != ans[i]) return;
+            }
+            answer++;
+            return;
+        }
+        
+        for(int i=start;i<=n;i++) {
+            selected[depth] = i;
+            comb(i+1, depth+1, selected, n, q, ans);
+        }
+    }
+    public int solution(int n, int[][] q, int[] ans) {
+        answer = 0;
+        comb(1, 0, new int[5], n, q, ans);
+        return answer;
+    }
+}
